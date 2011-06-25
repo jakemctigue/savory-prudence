727135fabe4ce0a8c38ec3c6b5a7e1eea8b56fc2
Tal Liron <tal.liron@gmail.com>
2011-06-25 04:45:12 +0000

    * Placeholders in demo app * Improve build script
    git-svn-id: https://savory-prudence.googlecode.com/svn/trunk@39 014a996a-f575-3a48-c0c3-b1b6f447b18a

--- modules/application/src/routing.js
+++ modules/application/src/routing.js
@@ -31,39 +31,3 @@ Savory.Processing.routing()
 Savory.PayPal.routing()
 Savory.Blocks.routing()
 Savory.HTML.routing()
-
-/*
-importClass(org.restlet.routing.Variable, com.threecrickets.prudence.DelegatedFilter)
-
-var uriPathVariable = new Variable(Variable.TYPE_URI_PATH)
-
-function createFilter(name, next) {
-	var filter = new DelegatedFilter(applicationInstance.context, name)
-	filter.next = next
-	return filter
-}
-
-function filterDynamicWeb(name, uri, base) {
-	if (uri) {
-		return base ? router.attachBase(uri, createFilter(name, dynamicWeb)) : router.attach(uri, createFilter(name, dynamicWeb))
-	}
-	else {
-		var filter = createFilter(name, dynamicWeb)
-		var route = router.filter(dynamicWebBaseURL, filter, dynamicWeb)
-		dynamicWeb = filter
-		return route
-	}
-}
-*/
-
-
-// uhoh !!! see /page/about/ !!!!
-//router.attachBase('page/', dynamicWeb)
-//router.reattachBase(dynamicWebBaseURL, dynamicWeb)
-
-//router.capture('/', 'page/home/')
-
-
-//router.capture('test/{id}/', 'data/?{rq}#{rp}', false)
-//router.capture('data/user/{id}/', 'data/?{rq}#user', false)
-//router.capture('data/users/', 'data/?{rq}#users', false)
